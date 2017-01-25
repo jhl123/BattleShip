8f1ddee7eb6411078d4633ed3a4a786e804570dc
Justin Lee <jhl12311@gmail.com>
2017-01-25 00:22:05 -0800

    fixed auto game creation bug

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -3,37 +3,39 @@ class GameController < ApplicationController
 
   def index
     @user = User.where(username: session[:username]).take
-    if !params[:game_id].nil?
-      @game = Game.find(params[:game_id])
-      if !@game.has_started && @user.id != @game.user_1_id
-        @game.add_user(@user.id)
-        @game.begin
-        Board.create_new_boards(@game)
-      end
-
-      if @game.has_started
-        if @user.id != @game.user_1_id && @user.id != @game.user_2_id
-          render :partial => "game/incorrect_user"
-          return
-        end
-
-
-        board_cache = Rails.cache.fetch('board_cache')
-        if board_cache.nil?
-          board_cache = Hash.new
-        end
-
-        if board_cache[@game.id].nil? || board_cache[@game.id][@user.id].nil?
-          add_to_cache(board_cache, @game)
-        end
-        @player_board = Rails.cache.fetch('board_cache')[@game.id][@user.id]
-        opponent_id = @game.user_1_id != @user.id ? @game.user_1_id : @game.user_2_id
-        @opponent_user = User.find(opponent_id)
-        @opponent_board = Rails.cache.fetch('board_cache')[@game.id][opponent_id]
-        @total_ships = Ship.count
-      end
-    else
+    if params[:game_id].nil?
       @game = Game.create(user_1_id: @user.id)
+      params[:game_id] = @game.id
+      return
+    end
+
+    @game = Game.find(params[:game_id])
+    if !@game.has_started && @user.id != @game.user_1_id
+      @game.add_user(@user.id)
+      @game.begin
+      Board.create_new_boards(@game)
+    end
+
+    if @game.has_started
+      if @user.id != @game.user_1_id && @user.id != @game.user_2_id
+        render :partial => "game/incorrect_user"
+        return
+      end
+
+
+      board_cache = Rails.cache.fetch('board_cache')
+      if board_cache.nil?
+        board_cache = Hash.new
+      end
+
+      if board_cache[@game.id].nil? || board_cache[@game.id][@user.id].nil?
+        add_to_cache(board_cache, @game)
+      end
+      @player_board = Rails.cache.fetch('board_cache')[@game.id][@user.id]
+      opponent_id = @game.user_1_id != @user.id ? @game.user_1_id : @game.user_2_id
+      @opponent_user = User.find(opponent_id)
+      @opponent_board = Rails.cache.fetch('board_cache')[@game.id][opponent_id]
+      @total_ships = Ship.count
     end
   end
 
